6e8e0c657ef7d5e0b1c1f918dfc3315fe7776114
Soocotra <mriziq54@gmail.com>
2021-10-25 20:45:44 +0700

    init

--- Array/foreachdanmap/foreachdanmap.js
+++ Array/foreachdanmap/foreachdanmap.js
@@ -1,3 +1,15 @@
+//forEach
 let nama = ['Muhammad', 'Riziq', 'Ramadhan'];
 
-nama.forEach(function(e, i));
+nama.forEach(function(e, i){
+    console.log('Mahasiswa ke-' + i + ' adalah: ' + e);
+})
+
+//MAP
+
+let angka = [1,2,3,4,5,6,7,8,9];
+
+let angka2 = angka.map(function(e){
+    return e*2 ;
+})
+console.log(angka2);
