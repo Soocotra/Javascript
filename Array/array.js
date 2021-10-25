eff4b8d7aa5196ba9479ce90093592281efd51fd
Soocotra <mriziq54@gmail.com>
2021-10-25 19:59:55 +0700

    init

--- Array/array.js
+++ Array/array.js
@@ -1,3 +1,25 @@
-let binatang = ['kucing','kelinci','monyet','panda','koala','sapi'];
+//Array Method
+let arr = ['Muhammad','Riziq','Ramadhan'];
+//Join
 
-console.log(binatang[0]);
+console.log('Method Join = ' + arr.join());
+//Length
+console.log('Mengetahui panjang array dengan Length pada Array');
+for (let i = 0; i <=arr.length; i++){
+    console.log(arr[i]);
+}   console.log("\n")
+
+//Push and Pop
+console.log('Menghapus array terakhir Pop: ');
+arr.pop();
+console.log(arr.join());
+console.log('\nMenambah Array dengan Push: ');
+arr.push('Al Arafah');
+console.log(arr.join());
+
+//Unshift dan shift
+console.log('\nMenambah dan mengurangi anggota pertama array: ');
+arr.shift();
+console.log(arr.join());
+arr.unshift();
+console.log(arr.join());
